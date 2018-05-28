a7b9700d3a29ba907c75a7fec8d616ababb876bd
IBASEIT\venkatasairamg <venkatasairamg@ibaseit.com>
2018-05-28 19:45:03 +0530

    try2

--- server/server.js
+++ server/server.js
@@ -6,10 +6,11 @@ const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(publicPath));
+
 app.get('/', function(req, res) {
    res.sendFile(publicPath+'/index.html');
 });
 
 app.listen(port, () => {
-  console.log(`Server is up on ${port} from sai`);
+  console.log(`Server is up `);
 });
